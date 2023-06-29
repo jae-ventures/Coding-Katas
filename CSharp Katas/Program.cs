1aa785e00aa81dddfcfb10a7fd9384e36a268621
J. Anthony <janthonycodesit@outlook.com>
2023-06-29 08:47:24 -0400

    maitenance: getting C# dev kit extension to work

---
+++ CSharp Katas/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args) {
+        helloWorld((args.Length > 0) ? args[0] : "World");
+    }
+
+
+    public static void helloWorld(string subject) {
+        Console.WriteLine($"Hello {subject}!");
+    }
+}
